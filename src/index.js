88f6d7a8c523e5118e488c41b233ad481211cea8
ShivaniKakde15 <kalyanikakade99@gmail.com>
2023-06-20 13:54:46 +0530

    save

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 
+import DisplayButton from './stateEx.js';
+// import ButtonToggle from './hook.js';
+// import TimePicker from './current_date_time.js';
+
+const root  = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<DisplayButton/>)
+
+// root.render(<ButtonToggle/>)
+// root.render(<TimePicker/>)
