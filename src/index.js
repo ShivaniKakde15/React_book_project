a9e62b5a95d0d800712d6655c609baed8c1bde8f
ShivaniKakde15 <kalyanikakade99@gmail.com>
2023-06-21 09:58:28 +0530

    save

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
+// import './index.js'
+import BookList from './App';
 
-import DisplayButton from './stateEx.js';
-// import ButtonToggle from './hook.js';
-// import TimePicker from './current_date_time.js';
 
-const root  = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<DisplayButton/>)
 
-// root.render(<ButtonToggle/>)
-// root.render(<TimePicker/>)
+
+
+// const root  = ReactDOM.createRoot(document.getElementById('root'));
+
+
+// root.render(
+// <BookList/>
+// );
+// export default BookList;
