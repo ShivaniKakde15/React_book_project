ce618dac78b13743a06d87ee907dc18aa5bf7bde
ShivaniKakde15 <kalyanikakade99@gmail.com>
2023-06-21 14:36:23 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import './index.css';
-// import './index.js'
 import BookList from './App';
 
+ const root  = ReactDOM.createRoot(document.getElementById('root'));
 
 
-
-
-// const root  = ReactDOM.createRoot(document.getElementById('root'));
-
-
-// root.render(
-// <BookList/>
-// );
-// export default BookList;
+root.render(
+<BookList/>
+);
